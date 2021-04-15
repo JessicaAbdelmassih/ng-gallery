f0053c3a0ca7c4d2ed8ada38a8fb6eb8a97cf244
jessica <jessicaabdelmassih@gmail.com>
2021-04-15 17:47:23 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+//import { HttpModule } from '@angular/http';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { GalleryComponent } from './gallery/gallery/gallery.component';
+import { ImageComponent } from './image/image/image.component';
+import { ImageService } from './image/image/shared/image.service';
+import { ImageFilterPipe } from './image/image/shared/filter.pipe';
+import { appRoutes } from '../routes'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GalleryComponent,
+    ImageComponent,
+    ImageFilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    //HttpModule,
+    AlertModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ImageService, ImageFilterPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
